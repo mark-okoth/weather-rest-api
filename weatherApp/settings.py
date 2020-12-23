1d643ff35538517e2b56d49944eacf64da5d36f2
mark odhiambo okoth <markokoth96@gmail.com>
2020-12-23 15:46:57 +0300

    models are done for the api

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weatherAppApi',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- weatherAppApi/models.py
+++ weatherAppApi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Weather(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField( max_length=50)
+    temperature = models.DecimalField(max_digits=2, decimal_places=1)
+    time =  models.TimeField(auto_now=False, auto_now_add=False)
+    day =models.DateField(auto_now=False, auto_now_add=False)
+    description = models.CharField(max_length=50)
+    
+    
