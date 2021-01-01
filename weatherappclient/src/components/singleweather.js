fa398d614ac675d9f771524bee6f6ae6ef4e4b38
mark odhiambo okoth <markokoth96@gmail.com>
2021-01-01 08:06:58 +0300

    deleting single weather working ok

--- weatherappclient/src/components/singleweather.js
+++ weatherappclient/src/components/singleweather.js
@@ -8,16 +8,27 @@ class Singleweather extends Component {
     this.state = {
       results: [],
     };
+    this.handleDelete = this.handleDelete.bind(this);
   }
   componentDidMount() {
     axios
       .get(`http://localhost:8000/api/weather/${this.props.match.params.id}/`)
       .then((res) => {
         const results = res.data;
-        this.setState({results: [results]})
+        this.setState({ results: [results] });
       })
       .catch((err) => {
         console.log(err);
+      });
+  }
+
+  handleDelete() {
+    axios
+      .delete(
+        `http://localhost:8000/api/weather/${this.props.match.params.id}/`
+      )
+      .then((res) => {
+        this.props.history.push('/')
       });
   }
 
@@ -26,21 +37,24 @@ class Singleweather extends Component {
       <div className="container col-5">
         <div className="card bg-success mt-5">
           <div className="card-body">
-          {
-            this.state.results.map((data, idx)=> 
-            <div className="List" key={idx }>
-              <ul className="list-group">
-                <li className="list-group-item">{data.name}</li>
-                <li className="list-group-item">{data.time}</li>
-                <li className="list-group-item">{data.description}</li>
-                <li className="list-group-item">{data.day}</li>
-                <li className="list-group-item">{data.location}</li>
-                <li className="list-group-item">{data.temperature}</li>
-              </ul>
-            </div>
-              
-            )
-          }
+            {this.state.results.map((data, idx) => (
+              <div className="List" key={idx}>
+                <ul className="list-group">
+                  <li className="list-group-item">{data.name}</li>
+                  <li className="list-group-item">{data.time}</li>
+                  <li className="list-group-item">{data.description}</li>
+                  <li className="list-group-item">{data.day}</li>
+                  <li className="list-group-item">{data.location}</li>
+                  <li className="list-group-item">{data.temperature}</li>
+                </ul>
+                <button
+                  className="btn btn-danger p-8 mt-2"
+                  onClick={this.handleDelete}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           </div>
         </div>
       </div>
